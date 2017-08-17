9aff1bc29bd331056ad291bc3b897a9b74001fab
Diego Torres <diegotorres50@hotmail.com>
2017-08-16 21:40:24 -0300

    implementando demo de carousels

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-
 // Para usar el look&feel ui bootstrap
 // https://github.com/ng-bootstrap/ng-bootstrap
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
+import { AppComponent } from './app.component';
+
+import { NgbdCarouselBasic } from './carousel-basic';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgbdCarouselBasic
   ],
   imports: [
     // Para usar el look&feel ui bootstrap
